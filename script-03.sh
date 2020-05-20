0797fc009e7847b8f607ca6a06eee00207593a98
Shawon Saha <shawondroid@gmail.com>
2020-05-21 01:27:28 +0600

    Extract and edit using Bash scripts
    Continuing your work for the Bulgarian soccer league - you need to do some editing on the data you have. Several teams have changed their names so you need to do some replacements. The data is the same as the previous exercise.
    
    You will need to create a Bash script that makes use of sed to change the required team names.

--- script-03.sh
+++ script-03.sh
@@ -3,4 +3,4 @@
 # Create a sed pipe to a new file
 cat soccer_scores.csv | sed 's/Cherno/Cherno City/g' | sed 's/Arda/Arda United/g' > soccer_scores_edited.csv
 
-# Now save and run!
+# Now save and run!!
